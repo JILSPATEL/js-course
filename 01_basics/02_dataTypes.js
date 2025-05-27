31ec65f23dfc74373afb7e02329c62eaa6c3c261
JILS PATEL <100358865+JILSPATEL@users.noreply.github.com>
2025-05-27 15:37:57 +0000

    create 02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+let name="jils"
+let age=22
+let isLoggedin=false
+
+// number => 2 to power 53
+// bigint
+// string => "" or ''
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof name);
+console.log(typeof null);
